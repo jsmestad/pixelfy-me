6e3ab20fda434240c14c594dace218cc85e093fd
Brandon Hays <brandon.hays@gmail.com>
2015-02-13 16:35:03 -0600

    generated images resource

---
+++ app/routes/images.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import Image from '../models/image';
+
+export default Ember.Route.extend({
+  model: function() {
+    return [
+      Image.create({url: '/img/flippin-sweet.jpg'})
+    ];
+  }
+});
